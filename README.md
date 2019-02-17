acf45405d36847b145fe8a7e1b561014db9830bc
Tyler Cromwell <tjc6185@gmail.com>
2019-02-17 12:21:54 -0500

    Building on top of Rustyline

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 Learning [Rust][rust] by writing a command-line shell.
 
 
+## Dependencies
+See `Cargo.toml`.
+
+
 [rust]: https://www.rust-lang.org/
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+extern crate rustyline;
+
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut rl = Editor::<()>::new();
+    //if rl.load_history("history.txt").is_err() {}
+
+    loop {
+        let input = rl.readline("rush$ ");
+        match input {
+            Ok(line) => {
+                //rl.add_history_entry(line.as_ref());
+            },
+            Err(ReadlineError::Interrupted) => {break},
+            Err(ReadlineError::Eof) => {break},
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break
+            }
+        }
+    }
+
+    //rl.save_history("history.txt").unwrap();
 }
