2d3b8745a4c38eb8ac1c62543e67a2fca9c70f95
Tyler Cromwell <tjc6185@gmail.com>
2019-02-23 23:36:45 -0500

    Moved Ok(line) code out of match input block

--- src/main.rs
+++ src/main.rs
@@ -17,64 +17,64 @@ fn main() {
     loop {
         let input = rl.readline("rush$ ");
 
-        match input {
-            Ok(line) => {
-                let line = line.trim();
-                rl.add_history_entry(line.as_ref());
-                let tokens = line.split(" ");
-                let tokens: Vec<&str> = tokens.collect();
-                let command = tokens[0].trim();
-                let home = dirs::home_dir().unwrap();
-
-                if command == "cd" {
-                    let directory;
-
-                    if tokens.len() == 1 {
-                        directory = home.to_str().unwrap();
-                    }
-                    else {
-                        if tokens[1] == "~" {
-                            directory = home.to_str().unwrap();
-                        } else {
-                            directory = tokens[1];
-                        }
-                    }
-
-                    match env::set_current_dir(directory) {
-                        Ok(_) => {},
-                        Err(err) => {
-                            eprintln!("{}: {}", directory, err);
-                        }
-                    }
-
-                    continue;
-                }
-                else if command == "exit" {
-                    break;
-                }
-                else if command == "" {
-                    continue;
-                }
-
-                let result = Command::new(command)
-                    .args(tokens[1..tokens.len()].into_iter())
-                    .output();
-
-                match result {
-                    Ok(result) => {
-                        stdout().write_all(&result.stdout).unwrap();
-                        stderr().write_all(&result.stderr).unwrap();
-                    },
-                    Err(err) => {
-                        eprintln!("{}: {}", command, err);
-                    }
-                }
-            },
-            Err(ReadlineError::Interrupted) => {},
-            Err(ReadlineError::Eof) => {break},
+        let line = match input {
+            Ok(line) => line,
+            Err(ReadlineError::Interrupted) => continue,
+            Err(ReadlineError::Eof) => break,
             Err(err) => {
                 eprintln!("Error: {:?}", err);
                 break
+            }
+        };
+
+        let line = line.trim();
+        rl.add_history_entry(line.as_ref());
+        let tokens = line.split(" ");
+        let tokens: Vec<&str> = tokens.collect();
+        let command = tokens[0].trim();
+        let home = dirs::home_dir().unwrap();
+
+        if command == "cd" {
+            let directory;
+
+            if tokens.len() == 1 {
+                directory = home.to_str().unwrap();
+            }
+            else {
+                if tokens[1] == "~" {
+                    directory = home.to_str().unwrap();
+                } else {
+                    directory = tokens[1];
+                }
+            }
+
+            match env::set_current_dir(directory) {
+                Ok(_) => {},
+                Err(err) => {
+                    eprintln!("{}: {}", directory, err);
+                }
+            }
+
+            continue;
+        }
+        else if command == "exit" {
+            break;
+        }
+        else if command == "" {
+            continue;
+        }
+
+        let result = Command::new(command)
+            .args(tokens[1..tokens.len()].into_iter())
+            .output();
+
+        match result {
+            Ok(result) => {
+                stdout().write_all(&result.stdout).unwrap();
+                stderr().write_all(&result.stderr).unwrap();
+            },
+            Err(err) => {
+                eprintln!("{}: {}", command, err);
             }
         }
     }
